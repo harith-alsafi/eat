7edc0c6c7aa7fb34eb93eac83a9d51da113fe46b
danrhul <75696965+danrhul@users.noreply.github.com>
2023-02-04 20:30:41 +0000

    Auth0

--- python/main.py
+++ python/main.py
@@ -1,9 +1,32 @@
 from dash import html, dcc
 from dash.dependencies import Input, Output
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
 
 from app import app
 from pages import home
 from components import navbar
+
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app.server.secret_key = env.get("APP_SECRET_KEY")
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
 
 nav = navbar.nav_bar()
 
@@ -13,16 +36,37 @@ app.layout = html.Div([
     html.Div(id='page-content'),
 ])
 
-@app.callback(
-    Output('page-content', 'children'),
-    Input('url', 'pathname')
-)
-def display_page(pathname):
-    match pathname:
-        case "/" | "/home":
-            return home.layout
-        case _:
-            return "404"
+@app.server.route("/")
+def home_page():
+    print("home")
+
+@app.server.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    app.server.session["user"] = token
+    return app.server.redirect("/")
+
+
+@app.server.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=app.server.url_for("callback", _external=True)
+    )
+
+
+@app.server.route("/logout")
+def logout():
+    app.server.session.clear()
+    app.server.redirect("https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": app.server.url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        ))
 
 if __name__ == '__main__':
     from sys import argv
