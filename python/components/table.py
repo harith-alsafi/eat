eae5b3eb75d416de1abf7325b0500e9fcb4a528f
Kumar Acharjee <kumaracharjee@outlook.com>
2023-02-05 10:19:59 +0000

    windows linux check

--- python/components/table.py
+++ python/components/table.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import dash_bootstrap_components as dbc
+import os
 
-matlab_data = pd.read_csv('../matlab/data.csv')
+if os.name == 'nt':
+    matlab_data = pd.read_csv("eat\matlab\data.csv")
+else:
+    matlab_data = pd.read_csv('../matlab/data.csv')
 old_table_view = dbc.Table.from_dataframe(matlab_data, striped=True, bordered=True, hover=True)
